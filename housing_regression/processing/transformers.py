5337e1bb3775a1758368169fcc97f220749e9309
Jan Helcl <jhelcl001@cee.ema.ad.pwcinternal.com>
2019-07-27 13:45:52 +0200

    RareLabelEncoder

--- housing_regression/processing/transformers.py
+++ housing_regression/processing/transformers.py
@@ -27,10 +27,7 @@ class TemporalDifferenceTransformer(BaseEstimator, TransformerMixin):
         self.temp_vars = temp_vars
         self.reference_var = reference_var
 
-    def fit(self,
-            X: pd.DataFrame,
-            y=None
-            ) -> 'TemporalDifferenceTransformer':
+    def fit(self, X, y=None):
         "For compatibility only"
         return self
 
@@ -41,6 +38,8 @@ class TemporalDifferenceTransformer(BaseEstimator, TransformerMixin):
         Calculates difference in time from a single reference variable.
         
         :param X: pd.DataFrame of model predictors
+        
+        :returns: Transformed data
         """
         X = X.copy()
         for feature in self.temp_vars:
@@ -53,7 +52,7 @@ class FeatureDropper(BaseEstimator, TransformerMixin):
     """Drops selected columns.
     
     Drops selected columns inside of a scikit-learn pipline. Useful for example
-    for features used fo feature engineering in previous steps that are
+    for features used for feature engineering in previous steps that are
     no longer needed.
     
     :param vars_to_drop: List of features to drop
@@ -62,6 +61,7 @@ class FeatureDropper(BaseEstimator, TransformerMixin):
     def __init__(self,
                  vars_to_drop: List[str] = None
                  ):
+
         self.vars_to_drop = vars_to_drop
 
     def fit(self, X, y=None):
@@ -72,8 +72,64 @@ class FeatureDropper(BaseEstimator, TransformerMixin):
         """Drops selected columns
         
         :param X: pd.DataFrame of model predictors
+        
+        :returns: Data without unwanted features
         """
         X = X.copy()
         X = X.drop(self.variables, axis=1)
+
+        return X
+
+
+class RareLabelEncoder(BaseEstimator, TransformerMixin):
+    """Rare label categorical encoder
+    
+    Merge all rare labels (proportin below selected tolerance) into one
+    category 'rare'. Previously unseen labels during transform are also
+    encoded as 'rare'.
+    
+    :param tol: Tolerance level, lables below this proportion will be merged
+    :param variables: List of variables to be encoded
+    """
+
+    def __init__(self, 
+                 tol:float = 0.0,
+                 variables: List[str] = None
+                 ):
+        
+        self.tol = tol
+        self.variables = variables
+
+    def fit(self,
+            X: pd.DataFrame,
+            y=None
+            ) -> 'RareLabelEncoder':
+        """Finds frequent categories
+        
+        :param X: pd.DataFrame of model predictors
+        :param y: For compatibility only
+        
+        :returns: self
+        """
+        self.frequent_labels_ = {}
+        for var in self.variables:
+            t = pd.Series(X[var].value_counts() / np.float(len(X)))
+            self.frequent_labels_[var] = list(t[t >= self.tol].index)
+
+        return self
+
+    def transform(self,
+                  X: pd.DataFrame
+                  ) -> pd.DataFrame:
+        """Encodes the infrequent labels to 'rare'
+        
+        :param X: pd.DataFrame of model predictors
+        
+        :returns: Encoded data
+        """
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = np.where(X[feature].isin(
+                self.frequent_labels_[feature]), X[feature], 'rare')
 
         return X
