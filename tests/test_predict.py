e99ac1da823013bc91f7fa0ad4517c73fa1f4516
Jan Helcl <jhelcl001@cee.ema.ad.pwcinternal.com>
2019-08-16 22:21:26 +0200

    test predict multiple observations

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -9,14 +9,30 @@ from housing_regression.predict import predict
 from housing_regression.processing.data_management import load_dataset
 
 
+TEST_DATA = '../housing_regression/data/test.csv'
+DEV_PIPELINE = '../housing_regression/trained_models/dev_pipe.pkl'
+
+
 def test_single_predict():
     """Can the dev pipeline predict a previously unseen observation?
     """
-    test_data = load_dataset('../housing_regression/data/test.csv')
+    test_data = load_dataset(TEST_DATA)
     single_json = test_data.iloc[[0]].to_json(orient='records')
-    scored = predict(single_json,
-                     '../housing_regression/trained_models/dev_pipe.pkl')
+    scored = predict(single_json, DEV_PIPELINE)
     
     assert scored is not None
     assert isinstance(scored['prediction'][0], float)
+    assert json.dumps(scored)
+    
+    
+def test_multiple_predict():
+    """Can the dev pipeline predict multiple unseen observations?
+    """
+    test_data = load_dataset(TEST_DATA)
+    single_json = test_data.to_json(orient='records')
+    scored = predict(single_json, DEV_PIPELINE)
+    
+    assert scored is not None
+    assert isinstance(scored['prediction'], list)
+    assert len(scored['prediction']) <= len(test_data)
     assert json.dumps(scored)
