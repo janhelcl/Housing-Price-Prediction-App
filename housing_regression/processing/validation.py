235b7fbc075a8632466255ad012af49add932487
Jan Helcl <jhelcl001@cee.ema.ad.pwcinternal.com>
2019-08-16 22:16:56 +0200

    first simple validation

---
+++ housing_regression/processing/validation.py
@@ -0,0 +1,25 @@
+"""
+Functionality to validate the imput data
+
+# TODO: extend to allow different pipes have different conditions
+"""
+import pandas as pd
+
+import housing_regression.config.dev_config as conf
+
+
+def validate_inputs(input_data: pd.DataFrame) -> pd.DataFrame:
+    """Filters out rows with missing values where not expected
+    
+    :param input_data: dataframe of data to be filtered
+    
+    :returns: dataframe with filtered rows
+    """
+
+    validated_data = input_data.copy()
+
+    if input_data[conf.NAN_NOT_ALLOWED].isnull().any().any():
+        validated_data = validated_data.dropna(
+            axis=0, subset=conf.NAN_NOT_ALLOWED)
+    
+    return validated_data
