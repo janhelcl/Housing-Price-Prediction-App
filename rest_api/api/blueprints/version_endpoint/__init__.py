21dd2761081c05ef220ac1e94583ef1308ed39c8
Jan Helcl <jhelcl001@cee.ema.ad.pwcinternal.com>
2019-08-18 11:48:44 +0200

    model version

--- rest_api/api/blueprints/version_endpoint/__init__.py
+++ rest_api/api/blueprints/version_endpoint/__init__.py
@@ -3,6 +3,7 @@ Endpoint returning version of the application and models
 """
 from flask import Blueprint, jsonify
 
+import housing_regression as hr
 
 version_endpoint = Blueprint('version_endpoint', __name__)
 
@@ -18,7 +19,6 @@ def get_api_version():
 def version():
     """Returns version of the application
     """
-    # TODO: add housing_regression version
     return jsonify({'api_version': get_api_version(),
-                    'models_version': None
+                    'models_version': hr.__version__
                     })
